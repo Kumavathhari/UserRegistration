4120f47d80a149210c5575b7ff75eaa9b7a550b0
Hari Naik <kumavathharinaik@gmail.com>
2024-07-15 14:45:06 +0530

    Welcome to User Registration Project

---
+++ src/main/java/org/example/UserRegistration.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Project.");
+
+    }
+}
+
