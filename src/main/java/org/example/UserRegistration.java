0a555797320f92017b4d3fd6e3a13753efd46e2e
Hari Naik <kumavathharinaik@gmail.com>
2024-07-15 14:49:42 +0530

    UC1 : First Name [Begin With Capital Letter & Must have 3 character

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Project.");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        if (firstName != null && firstName.matches("^[A-Z][a-zA-Z]{2,}$")) {
+            System.out.println("First name " + firstName + " is valid.");
+        } else {
+            System.out.println("First name " + firstName + " is invalid. The first name must start with a capital letter and have a minimum of 3 characters.");
+        }
     }
-}
+    }
 
