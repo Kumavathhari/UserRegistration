0436e904844fe0d16b8af330b1a1e63b1083f712
Hari Naik <kumavathharinaik@gmail.com>
2024-07-15 14:58:46 +0530

    UC2 : Last Name [Begin With Capital Letter & Must have 3 character

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -6,13 +6,13 @@ public class UserRegistration {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your first name: ");
-        String firstName = scanner.nextLine();
+        System.out.print("Enter your last name: ");
+        String lastName = scanner.nextLine();
 
-        if (firstName != null && firstName.matches("^[A-Z][a-zA-Z]{2,}$")) {
-            System.out.println("First name " + firstName + " is valid.");
+        if (lastName != null && lastName.matches("^[A-Z][a-zA-Z]{2,}$")) {
+            System.out.println("Last name " + lastName + " is valid.");
         } else {
-            System.out.println("First name " + firstName + " is invalid. The first name must start with a capital letter and have a minimum of 3 characters.");
+            System.out.println("Last name " + lastName + " is invalid. The last name must start with a capital letter and have a minimum of 3 characters.");
         }
     }
     }
