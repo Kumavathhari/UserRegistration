0d4099e5689bd03430296beb9cd859f300149cb0
Hari Naik <kumavathharinaik@gmail.com>
2024-07-17 13:22:55 +0530

    UC11 : Junit Parameterised multiple Emails entry

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,57 +1,71 @@
 import org.example.UserRegistration;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-
-import org.junit.Test;
-
-    public class UserRegistrationTest {
+public class UserRegistrationTest {
 
         @Test
         public void validFirstNameTest() {
             Boolean result = UserRegistration.isValidFirstName("Hari");
-            Assert.assertTrue(result);
+            Assertions.assertTrue(result);
         }
 
         @Test
         public void inValidFirstNameTest() {
             Boolean result = UserRegistration.isValidFirstName("hari");
-            Assert.assertFalse(result);
+            Assertions.assertFalse(result);
         }
 
         @Test
         public void validLastNameTest() {
             Boolean result = UserRegistration.isValidFirstName("Naik");
-            Assert.assertTrue(result);
+            Assertions.assertTrue(result);
         }
 
         @Test
         public void inValidLastNameTest() {
             Boolean result = UserRegistration.isValidFirstName("naik");
-            Assert.assertFalse(result);
+            Assertions.assertFalse(result);
         }
 
         @Test
         public void validEmailTest() {
             Boolean result = UserRegistration.isValidEmail("harinaik@gmail.com");
-            Assert.assertTrue(result);
+            Assertions.assertTrue(result);
         }
 
         @Test
         public void inValidEmailTest() {
             Boolean result = UserRegistration.isValidEmail("Harikddfnvodn");
-            Assert.assertFalse(result);
+            Assertions.assertFalse(result);
+        }
+        @ParameterizedTest
+        @ValueSource(strings = {"Hari.Naik007@gmail.com", "Good.007@gmail.com", "Harry.007Naik@gmail.com"})
+        public void validEmailTest(String mail) {
+            Boolean result = UserRegistration.isValidEmail(mail);
+            Assertions.assertTrue(result);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"Hari#gmail.com",
+                "Harigmail.com", "205Hari@gmail@com"})
+        public void inValidEmailTest(String mail) {
+            Boolean result = UserRegistration.isValidEmail(mail);
+            Assertions.assertFalse(result);
         }
 
         @Test
         public void validMobileTest() {
             Boolean result = UserRegistration.isValidMobileNumber("91 9123456789");
-            Assert.assertTrue(result);
+            Assertions.assertTrue(result);
         }
 
         @Test
         public void inValidMobileTest() {
             Boolean result = UserRegistration.isValidMobileNumber("9123456789");
-            Assert.assertFalse(result);
+            Assertions.assertFalse(result);
         }
 
     }
